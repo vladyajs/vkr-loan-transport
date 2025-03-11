00279e475de95357256a289c5d9452e99c53e24e
Владислав Андреевич Шипай <shipay@atb.su>
2025-03-11 20:48:47 +0300

    initial commit

---
+++ src/test/java/com/vkr/vkr_loan_transport/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkr.vkr_loan_transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
